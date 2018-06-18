6ecf8beaafc3b6b2923ef2b41c3671c6ce47aa83
Kristoffer Katra <doedmannen@aetmej.se>
2018-06-18 14:19:03 +0200

    .\cloud.js

---
+++ cloud.js
@@ -0,0 +1,22 @@
+class cloud{
+
+  constructor(){
+    this.r = itemScale*4;
+    this.x = random(0, windowWidth);
+    this.y = random(rectH*0.2, windowHeight-rectH-this.r);
+
+  }
+
+  show(){
+      image(i_cloud, this.x, this.y, this.r*2, this.r);
+  }
+
+  move(){
+    this.x -= 2*speedScale*levelSpeedItem;
+    if(this.x < -this.r*2){
+        this.x = windowWidth+this.r;
+        this.y = random(rectH*0.2, windowHeight-rectH-this.r);
+      }
+  }
+
+}
