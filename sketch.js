e0b9fc57a9ba0fc866cad6cb919c83144e0f6c48
Kristoffer Katra <doedmannen@aetmej.se>
2018-07-04 15:40:53 +0200

    Sketch

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+// Is user is on mobile or computer
+let mobile = checkMobile();
+
 // Player variables
 let player;
 let moveDir = 0;
@@ -26,6 +29,7 @@ let itemCount = 0;
 let i_soda;
 let i_burger;
 let i_fries;
+let i_rocket;
 
 let i_bomb;
 let i_explode;
@@ -44,19 +48,24 @@ function preload(){
   i_soda = loadImage("img/soda.png");
   i_burger = loadImage("img/burger.png");
   i_fries = loadImage("img/fries.png");
+  i_rocket = loadImage("img/rocket.png");
   i_bomb = loadImage("img/bomb.png");
   i_explode = loadImage("img/explode.png");
 
 
   i_frog = loadImage("img/frog.png");
   i_frogdead = loadImage("img/frogdead.png");
+  i_frogboost = loadImage("img/frogboost.png");
   i_cloud = loadImage("img/cloud.png");
 
   i_up = loadImage("img/up.png");
   i_down = loadImage("img/down.png");
-  i_pause = loadImage("img/pause.png");
 
-  gameFont = loadFont('font/AgencyFBBoldCondensed.ttf')
+  gameFont = loadFont('font/Anton-Regular.ttf')
+}
+
+function functionName() {
+
 }
 
 function setup() {
@@ -67,7 +76,7 @@ function setup() {
   itemScale = windowHeight*0.05;  	     // Set scale for items
   speedScale = windowWidth/1500;         // Set scale for speed
   // Set scale for playerspeed
-  playerMoveSpeed = (windowHeight-rectH*0.2-rectH)/120;
+  playerMoveSpeed = (windowHeight-rectH*0.2-rectH)/90;
 
   frameRate(100);                        // Probably not going to do 100fps
 
@@ -80,9 +89,9 @@ function setup() {
   strokeJoin(ROUND);
   textAlign(CENTER);
 
-  largeText = 1*(rectH*0.6).toFixed(0);
-  mediumText = 1*(rectH*0.4).toFixed(0);
-  smallText = 1*(rectH*0.2).toFixed(0);
+  largeText = 1*(rectH*0.5).toFixed(0);
+  mediumText = 1*(rectH*0.3).toFixed(0);
+  smallText = 1*(rectH*0.18).toFixed(0);
 
 }
 
@@ -109,11 +118,11 @@ function windowResized() {
   rectH = windowHeight*0.2;              // Set scale for bar
   itemScale = windowHeight*0.05;  	     // Set scale for items
   speedScale = windowWidth/1500;         // Set scale for speed
-  playerMoveSpeed = (windowHeight-rectH*0.2-rectH)/120;
+  playerMoveSpeed = (windowHeight-rectH*0.2-rectH)/90;
 
-  largeText = 1*(height/10).toFixed(0);
-  mediumText = 1*(height/15).toFixed(0);
-  smallText = 1*(height/20).toFixed(0);
+  largeText = 1*(rectH*0.5).toFixed(0);
+  mediumText = 1*(rectH*0.3).toFixed(0);
+  smallText = 1*(rectH*0.18).toFixed(0);
 
   player.r = itemScale;
   player.speed = playerMoveSpeed;
@@ -146,35 +155,43 @@ function draw(){
 
 
   if(coin){
+
+    clouds.show();
+    clouds.move();
+
     count++;
     if(count > 249){
-      if(levelSpeedItem < 6){
+      if((levelSpeedItem < 3 || levelSpeedPlayer < 3) && player.rocketTimer < 1){
         levelSpeedItem = 1*(levelSpeedItem+0.1).toFixed(2);
-      }
-      if(levelSpeedPlayer < 3){
-        levelSpeedPlayer = 1*(levelSpeedPlayer+0.05).toFixed(2);
+        levelSpeedPlayer = 1*(levelSpeedPlayer+0.1).toFixed(2);
       }
       count = 0;
     }
 
+    if(player.rocketTimer > 0){
+      background(50, 50, 150);
+      clouds.show();
+      clouds.move();
+    }
 
-    /*
-    Öka levelSpeedItem och levelSpeedPlayer var % 5000
-    Item bör öka mer än player.
-    Öka tills hastigheten på player motsvarar värdet*2
-    och item.speed = 25. Stanna därefter.
-    Undersök när i count detta inträffar och nollställ count varje gång
-    ett sånt helvarv inträffar, så att vi inte råkar på en maxvalue-bug.
-    */
+    if(player.rocket > 0){
+      textSize(mediumText);
+      strokeWeight(mediumText/20);
+      stroke(0);
+      fill(255);
+      text("USE ROCKET BOOSTER", windowWidth/2, windowHeight-rectH*1.1);
+    }
 
     if(count % 250 == 0){
       itemCount++;
-      if(itemCount > 10){
+      if(itemCount > 14){
         itemCount = 1;
       }
-      if(itemCount == 10){
+      if(itemCount == 8){
+        items[items.length] = new item("ROCKET");
+      } else if(itemCount == 10){
         items[items.length] = new item("SODA");
-      } else if(itemCount == 3 || itemCount == 6 || itemCount == 9){
+      } else if(itemCount == 2 || itemCount == 5 || itemCount == 11 || itemCount == 14){
         items[items.length] = new item("FRIES");
       } else {
         items[items.length] = new item("BURGER");
@@ -183,46 +200,31 @@ function draw(){
     if(levelSpeedItem < 2 && count % 100 == 0 && count % 250 != 0){
       items[items.length] = new item();
     }
-    if(levelSpeedItem >= 2 && levelSpeedItem < 4 &&
+    if(levelSpeedItem >= 2 && levelSpeedItem < 3 &&
       count % 50 == 0 && count % 250 != 0){
       items[items.length] = new item();
-    }
-    if(levelSpeedItem >= 4 && count % 25 == 0 && count % 250 != 0){
       items[items.length] = new item();
     }
-
-    // Controlls for mobile game
-    if(game && mouseIsPressed){
-      moveCheck();
+    if(levelSpeedItem >= 3 && count % 25 == 0 && count % 250 != 0){
+      items[items.length] = new item();
+      if(count > 150){
+        items[items.length] = new item();
+      }
     }
+
+
 
     // Controlls for computer-testing
-    if(game && keyIsDown(40)){
+    if(game && keyIsDown(81)){
       moveDir = +1;
     }
-    if(game && keyIsDown(38)){
+    if(game && keyIsDown(80)){
       moveDir = -1;
     }
 
-    clouds.show();
-    clouds.move();
 
 
 
-    fill(0);
-    noStroke();
-    rect(0, windowHeight-rectH, windowWidth, rectH);
-    rect(0, 0, windowWidth, rectH*0.2);
-
-    image(i_down, windowWidth*0.2, windowHeight-rectH+itemScale, itemScale*2, itemScale*2);
-    image(i_up, windowWidth*0.7, windowHeight-rectH+itemScale, itemScale*2, itemScale*2);
-
-    fill(255);
-    textSize(smallText);
-    textStyle(BOLD);
-
-    text("Score: " + score, width/2, rectH*0.2-1);
-    // text("Speed: " + levelSpeedItem, 100, rectH*0.2-5);
 
 
     // Move and draw our items
@@ -239,21 +241,51 @@ function draw(){
     player.show();
 
 
+
     if(pause){
       textSize(largeText);
       stroke(0);
       fill(255);
-      strokeWeight(1*(largeText/20).toFixed(0));
+      strokeWeight(largeText/20);
       text("P A U S E D", width/2, height/2-rectH*0.6);
 
     }
+
+    if(!game){
+      gameOver();
+    }
+
+    fill(0);
+    noStroke();
+    rect(0, windowHeight-rectH, windowWidth, rectH);
+    rect(0, 0, windowWidth, rectH*0.2);
+
+    if(mobile){
+      image(i_down, windowWidth*0.2, windowHeight-rectH+itemScale, itemScale*2, itemScale*2);
+      image(i_up, windowWidth*0.7, windowHeight-rectH+itemScale, itemScale*2, itemScale*2);
+    } else {
+      textSize(mediumText);
+      fill(255,255,0);
+      text("Q = DOWN", windowWidth*0.1, windowHeight-largeText*0.5);
+      text("P = UP", windowWidth*0.9, windowHeight-largeText*0.5);
+      if(player.rocket > 0){
+        text("SPACE = ROCKET", windowWidth/2, windowHeight-largeText*0.5);
+      }
+    }
+
+
+    fill(255);
+    textSize(smallText);
+    textStyle(BOLD);
+
+    text("Score: " + score, width/2, rectH*0.2-1);
+
+
   } else {
     gameMenu();
   }
 
-  if(!game){
-    gameOver();
-  }
+
 
 
 }
