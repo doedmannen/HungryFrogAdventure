587aa268deb2c1c22606cb0b858e55e161a32eba
Kristoffer Katra <doedmannen@aetmej.se>
2018-07-04 15:40:11 +0200

    Items

--- item.js
+++ item.js
@@ -6,13 +6,15 @@ class item{
     this.r = itemScale;
     this.x = windowWidth+this.r*2;
     this.y = random(rectH*0.2+this.r, windowHeight-rectH-this.r);
-    this.speed = 5;
+    this.speed = 10;
     if(this.type == "SODA"){
       this.img = i_soda;
     } else if(this.type == "FRIES"){
       this.img = i_fries;
     } else if(this.type == "BURGER"){
       this.img = i_burger;
+    } else if(this.type == "ROCKET"){
+      this.img = i_rocket;
     }
 
 
@@ -24,11 +26,42 @@ class item{
 
   checkType(){
     if(this.type == "BURGER"){
-      score += 10;
+      if(player.rocketTimer > 0){
+        score += 100;
+        player.lastP = "+100 points";
+        player.lastPC = 50;
+      } else {
+        score += 20;
+        player.lastP = "+20 points";
+        player.lastPC = 50;
+      }
+
     } else if (this.type == "FRIES"){
-      score += 50;
+      if(player.rocketTimer > 0){
+        score += 250;
+        player.lastP = "+250 points";
+        player.lastPC = 50;
+      } else {
+        score += 50;
+        player.lastP = "+50 points";
+        player.lastPC = 50;
+      }
+
     } else if (this.type == "SODA"){
-      score *=2;
+      if(player.rocketTimer > 0){
+        score += 500;
+        player.lastP = "+500 points";
+        player.lastPC = 50;
+      } else {
+        score += 100;
+        player.lastP = "+100 points";
+        player.lastPC = 50;
+      }
+
+    } else if (this.type == "ROCKET"){
+      player.rocket = 1;
+      player.lastP = "ROCKET BOOSTER";
+      player.lastPC = 50;
     } else {
       player.explodeX = this.x-this.r*1.5;
       player.explodeY = this.y-this.r;
@@ -50,7 +83,7 @@ class item{
   }
 
   move(){
-    this.x -= this.speed*speedScale*levelSpeedItem;
+    this.x -= this.speed*speedScale*levelSpeedItem*player.rocketSpeed;
     if(this.intersects()){
       this.checkType();
       this.y = -100;
